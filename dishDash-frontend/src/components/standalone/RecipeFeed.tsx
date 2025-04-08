82b3d457c5bae4e5c516f20aad08f0239031b227
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-04-08 21:25:19 +0530

    style: adjusted RecipeFeed spacing and typography

--- dishDash-frontend/src/components/standalone/RecipeFeed.tsx
+++ dishDash-frontend/src/components/standalone/RecipeFeed.tsx
@@ -23,7 +23,7 @@ export default function RecipeFeed({ recipes }: RecipeFeedProps) {
   }
 
   return (
-    <div className="max-w-screen-xl mx-auto px-4 pb-16">
+    <div className="max-w-screen-xl mx-auto pt-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {recipes.map((recipe) => (
           <RecipeCard key={recipe.id} recipe={recipe} />
